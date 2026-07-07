844ee0a1235e3c4203bf41c3ff033ce676d04bec
Maundytime <maundy.time@gmail.com>
2026-07-08 03:57:43 +0800

    Refactor example node env

--- await-widget/examples/xo.config.js
+++ await-widget/examples/xo.config.js
@@ -1,31 +1,20 @@
 export default [
 	{
-		ignores: ['**/simple-icons.json', '**/*.{js,mjs,cjs}'],
-	},
-	{
-		files: ['**/*.tsx'],
+		files: ['**/*.{tsx,ts}'],
 		languageOptions: {
 			globals: {
 				$: 'readonly',
 			},
 		},
 		rules: {
-			'@stylistic/padding-line-between-statements': 'off',
-			'@stylistic/no-mixed-operators': 'off',
-			'@stylistic/object-curly-spacing': 'off',
-			'@stylistic/semi-spacing': 'off',
-			'@stylistic/no-extra-semi': 'off',
-			'@stylistic/eol-last': 'off',
 			'@stylistic/max-len': 'off',
+			'@stylistic/no-mixed-operators': 'off',
 			'@typescript-eslint/naming-convention': 'off',
-			'@typescript-eslint/no-unnecessary-type-assertion': 'off',
-			'capitalized-comments': 'off',
-			'import-x/extensions': 'off',
-			'unicorn/prevent-abbreviations': 'off',
-			'unicorn/no-empty-file': 'off',
+			'@typescript-eslint/strict-boolean-expressions': 'off',
 			'require-unicode-regexp': 'off',
-			'@typescript-eslint/no-unsafe-type-assertion': 'off',
-			'unicorn/no-array-sort': 'off',
+			'unicorn/consistent-boolean-name': 'off',
+			'unicorn/filename-case': 'off',
+			'unicorn/name-replacements': 'off',
 		},
 	},
 ];
